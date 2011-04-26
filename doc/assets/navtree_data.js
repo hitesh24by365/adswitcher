b3f9bc901b905c0b3380d7475a47a0b987bed429
Cristian <casidiablo@elhacker.net>
2011-04-26 08:59:57 -0500

    + Javadoc reference in Html using doclava

---
+++ doc/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "in.adswitcher.android", "in/adswitcher/android/package-summary.html", [ [ "Description", "in/adswitcher/android/package-descr.html", null, "" ], [ "Classes", null, [ [ "AdConfiguration", "in/adswitcher/android/AdConfiguration.html", null, "" ], [ "AdConfiguration.Builder", "in/adswitcher/android/AdConfiguration.Builder.html", null, "" ], [ "AdHolder", "in/adswitcher/android/AdHolder.html", null, "" ], [ "AdParameter", "in/adswitcher/android/AdParameter.html", null, "" ], [ "AdSettings", "in/adswitcher/android/AdSettings.html", null, "" ], [ "AdSwitcher", "in/adswitcher/android/AdSwitcher.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "in.adswitcher.android.template", "in/adswitcher/android/template/package-summary.html", [ [ "Description", "in/adswitcher/android/template/package-descr.html", null, "" ], [ "Classes", null, [ [ "Placeholder", "in/adswitcher/android/template/Placeholder.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ doc/reference/lists.js
@@ -0,0 +1,12 @@
+var DATA = [
+      { id:0, label:"in.adswitcher.android", link:"in/adswitcher/android/package-summary.html", type:"package" },
+      { id:1, label:"in.adswitcher.android.AdConfiguration", link:"in/adswitcher/android/AdConfiguration.html", type:"class" },
+      { id:2, label:"in.adswitcher.android.AdConfiguration.Builder", link:"in/adswitcher/android/AdConfiguration.Builder.html", type:"class" },
+      { id:3, label:"in.adswitcher.android.AdHolder", link:"in/adswitcher/android/AdHolder.html", type:"class" },
+      { id:4, label:"in.adswitcher.android.AdParameter", link:"in/adswitcher/android/AdParameter.html", type:"class" },
+      { id:5, label:"in.adswitcher.android.AdSettings", link:"in/adswitcher/android/AdSettings.html", type:"class" },
+      { id:6, label:"in.adswitcher.android.AdSwitcher", link:"in/adswitcher/android/AdSwitcher.html", type:"class" },
+      { id:7, label:"in.adswitcher.android.template", link:"in/adswitcher/android/template/package-summary.html", type:"package" },
+      { id:8, label:"in.adswitcher.android.template.Placeholder", link:"in/adswitcher/android/template/Placeholder.html", type:"class" }
+
+    ];
